b1268552e8804227c0e8b0f3a25943ecb4cdcb65
TuziMoe <a@tuzi.moe>
2019-08-29 09:42:28 +1200

    :sparkles: 首页 header

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import {backToTop} from 'element-ui'
+import '../theme/index.css'
+//import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+
+
 
 Vue.use(ElementUI)
 
